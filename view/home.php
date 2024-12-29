a2b52d007e027b775325e785808ba6d1e226fde3
suwantara <suwantaraputra1@gmail.com>
2024-12-29 16:06:00 +0800

    menghapus menu tour

--- view/home.php
+++ view/home.php
@@ -9,7 +9,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
     <?php require_once "../application/controller/destination.php";?>
     <?php require_once "../application/controller/rental.php";?>
-    <?php require_once "../application/controller/tour.php";?>
     <?php require_once "../application/controller/review.php";?>
 
 </head>
